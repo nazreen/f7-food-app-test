25a67cd978e1c6084c0bc537caaeb6f7c8937d93
Nazreen Mohamad <nxmohamad@gmail.com>
2020-09-11 16:39:28 +0100

    setup f7 app

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <f7-app :params="$root.f7params">
   <main>
     <div v-if="this.appState === 'LOADING'">loading...</div>
     <div v-else>
@@ -7,6 +8,7 @@
       </div>
     </div>
   </main>
+  </f7-app>
 </template>
 
 <script>
@@ -14,6 +16,7 @@ import { getProducts } from './services/api'
 import { APP_STATES } from './constants'
 import ProductCard from './components/ProductCard.vue'
 // import Framework7 from "framework7"
+import { f7ready } from 'framework7-vue';
 
 export default {
   name: 'App',
@@ -29,8 +32,11 @@ export default {
     this.appState = APP_STATES.LOADING
     this.products = await getProducts()
     this.appState = APP_STATES.READY
-    // const app = new Framework7({ name: 'toast-app'})
-    // this.toast = app.toast
+  },
+  mounted() {
+    f7ready(() => {
+      console.log('f7 is ready')
+    })
   }
 }
 </script>
