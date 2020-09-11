7c32d91bb6395ecde81c0126561cf0582b6e6b68
Nazreen Mohamad <nxmohamad@gmail.com>
2020-09-11 15:01:13 +0100

    init productcard

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <article>
+    
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'ProductCard',
+  props: {
+    product: Object
+  },
+  created() {
+    console.log(this.product)
+  }
+}
+</script>
+
+<style>
+
+</style>
