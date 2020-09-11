108e85b03c95a0b6eee93ca9845880ba32fffc63
Nazreen Mohamad <nxmohamad@gmail.com>
2020-09-11 17:03:14 +0100

    use https

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export async function getProducts() {
   try {
-    return (await axios.get("http://xinnature.asia/menu/wp-json/wc/store/products")).data
+    return (await axios.get("https://xinnature.asia/menu/wp-json/wc/store/products")).data
   } catch(e) {
     console.error('failed to get products list from API')
     console.error(e)
